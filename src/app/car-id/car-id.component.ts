32d716192417b74efdce2557d3b434e27a57743b
Rokas Gulbinas <rkgulbinas@gmail.com>
2019-06-29 13:04:05 +0300

    Add car identification page component's html layout

---
+++ src/app/car-id/car-id.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-id',
+  templateUrl: './car-id.component.html',
+  styleUrls: ['./car-id.component.css']
+})
+export class CarIdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
